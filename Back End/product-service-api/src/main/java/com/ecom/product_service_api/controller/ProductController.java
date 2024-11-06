f0fac7e41bdb82aae3529265dfc345063f6086c5
harshani <ruwanthikapemarathna@gmail.com>
2024-11-06 19:38:43 +0530

    Completed Product CRUD

--- Back End/product-service-api/src/main/java/com/ecom/product_service_api/controller/ProductController.java
+++ Back End/product-service-api/src/main/java/com/ecom/product_service_api/controller/ProductController.java
@@ -10,10 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -36,5 +33,46 @@ public class ProductController {
                 HttpStatus.CREATED
         );
     }
+
+    @PutMapping("/update/{productId}")
+    public ResponseEntity<StandardResponse> updateProduct(
+            @RequestBody RequestProductDto productDto , @PathVariable String productId){
+               productService.updateProduct(productDto, productId);
+               return new ResponseEntity<>(
+                       new StandardResponse(201,null,"Product Updated.."),
+                       HttpStatus.CREATED
+               );
+    }
+
+    @DeleteMapping("/delete/productId")
+    public ResponseEntity<StandardResponse> deleteProduct(@PathVariable String productId){
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(
+                new StandardResponse(204,"null","Product Deleted.."),
+                HttpStatus.NO_CONTENT
+        );
+    }
+
+    @GetMapping("/find-by-id/{productId}")
+    public ResponseEntity<StandardResponse> findProductById(@PathVariable String productId){
+
+        return new ResponseEntity<>(
+                new StandardResponse(200,"productService.findProductById(productId)","Product Data.."),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/search-all-products")
+    public ResponseEntity<StandardResponse> searchAllProducts(
+            @RequestParam String searchText, @RequestParam int page, @RequestParam int size ){
+
+            return new ResponseEntity<>(
+                    new StandardResponse(200,"productService.searchAllProducts(searchText,page,size)","Product List.."),
+                    HttpStatus.OK
+            );
+    }
+
+    public void updateImage(MultipartFile file, String imageId);
+    public void deleteImage(String imageId);
 
 }
