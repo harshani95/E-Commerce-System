31b9c708a4c72dbad322c8b194164f0897c6a182
harshani <ruwanthikapemarathna@gmail.com>
2024-11-23 23:20:16 +0530

    Configured Eureka Client

--- Back End/order-payment-api/index.js
+++ Back End/order-payment-api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const OrderRoute = require("./route/OrderRoute");
+const { Eureka } = require('eureka-js-client');
 
 const app = express();
 
@@ -10,12 +11,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/order", OrderRoute);
 
+// Eureka client configuration
+const client = new Eureka({
+  instance: {
+      app: 'order-payment-service',
+      hostName: 'localhost',
+      ipAddr: '127.0.0.1',
+      port: { '$': 3001, '@enabled': true },
+      vipAddress: 'order-payment-service',
+      dataCenterInfo: {
+          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+          name: 'MyOwn',
+      },
+  },
+  eureka: {
+      host: 'localhost',
+      port: 8761,
+      servicePath: '/eureka/apps/',
+  },
+});
+
+
 mongoose.connect("mongodb://localhost:27017/ecommerce", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
     .catch(error => console.log('MongoDB connection error:', error));
+
+// Eureka client start
+client.start((error) => {
+  if (error) {
+      console.log('Eureka registration failed:', error);
+  } else {
+      console.log('Eureka registration successful!');
+  }
+});
 
 
 app.listen(3000, () => {
